8be2b070765eaaa84f528b3a64352be3b56be93e
Martin Grolmus <martin@stagro.sk>
2024-12-17 13:15:42 +0100

    ~ refactoring for pimcore 11 - JS registration

--- src/DivanteTranslationBundle/DivanteTranslationBundle.php
+++ src/DivanteTranslationBundle/DivanteTranslationBundle.php
@@ -12,14 +12,4 @@ use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
 
 final class DivanteTranslationBundle extends AbstractPimcoreBundle
 {
-    public function getJsPaths(): array
-    {
-        return [
-            '/bundles/divantetranslation/js/pimcore/startup.js',
-            '/bundles/divantetranslation/js/pimcore/object/tags/input.js',
-            '/bundles/divantetranslation/js/pimcore/object/tags/wysiwyg.js',
-            '/bundles/divantetranslation/js/pimcore/object/tags/textarea.js',
-            '/bundles/divantetranslation/js/pimcore/object/elementservice.js',
-        ];
-    }
 }
